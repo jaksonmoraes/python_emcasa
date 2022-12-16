9eeb9fb8b0fe42099198d1d02fc8d232a86db1a5
Projeto Publico - Evento <jakson.luis@hotmail.com>
2022-12-16 12:17:12 -0300

    segue ...

--- teste_python/control/controla_venda.py
+++ teste_python/control/controla_venda.py
@@ -2,8 +2,27 @@ from view.tela_pessoa import TelaPessoa
 from model.pessoa import Pessoa
 from view.tela_carro import TelaCarro
 from model.carro import Carro
+from control.controla_carro import ControlaCarro
+from view.tela_venda import TelaVenda
 
 
 class ControlaVenda:
     def __init__(self):
-        pass
+        self.__vendas = []
+        self.__tela_pessoa = TelaPessoa()
+        self.__tela_carro = TelaCarro()
+        self.__controlador_sistema = controlador_sistema
+        self.__mostra_carros = mostra_carros
+    
+    def lista_carros_disponiveis(self):
+        self.__mostra_carros.lista_carros()
+    
+    def retornar(self):
+        self.__controlador_sistema.abre_tela()
+    
+    def abre_tela(self):
+        lista_opcoes = {1:self.lista_carros_disponiveis, 0:self.retornar}
+
+        continua = True
+        while continua:
+            lista_opcoes[self.tela_venda.tela_opcoes()]()
---
+++ teste_python/view/tela_venda.py
@@ -0,0 +1,10 @@
+class TelaVenda:
+    def tela_opcoes(self):
+        print("----- TELA VENDA -----")
+        print("Qual sua opcao")
+        print("1 - Listar carros")
+        print("0 - Retornar")
+
+        opcao = int(input("Qual sua opção na tela de vendas: "))
+        return opcao
+    
