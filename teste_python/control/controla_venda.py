d0a9c1535d64ec390db7e4bae8c080d15c4f4e34
Projeto Publico - Evento <jakson.luis@hotmail.com>
2022-12-16 11:57:48 -0300

    desenvolvendo  controller de vendas

--- teste_python/control/controla_venda.py
+++ teste_python/control/controla_venda.py
@@ -1,3 +1,9 @@
+from view.tela_pessoa import TelaPessoa
+from model.pessoa import Pessoa
+from view.tela_carro import TelaCarro
+from model.carro import Carro
+
+
 class ControlaVenda:
     def __init__(self):
         pass
